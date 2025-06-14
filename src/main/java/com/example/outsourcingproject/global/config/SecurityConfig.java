13a81058270ac5a5d3c6fe32e00078895e08e925
Mybread2 <ckwnsgh4@naver.com>
2025-06-14 23:06:14 +0900

    feat: 프론트엔드랑 연동 확인
    - cors 설정
    - properties 에서 /api/v1 -> /api 로 수정
    (프론트엔드 js 확인해본 결과, url이 /api로 설정됨)

--- src/main/java/com/example/outsourcingproject/global/config/SecurityConfig.java
+++ src/main/java/com/example/outsourcingproject/global/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/signup",
-                                "/auth/signin"
+                                "/auth/signin",
+                                "/auth/refresh"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
