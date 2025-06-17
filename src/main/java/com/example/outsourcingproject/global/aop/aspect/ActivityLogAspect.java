95b50c228886a67b522b05d1836634e2f47289fe
huouvcti <huouvcti@gamil.com>
2025-06-17 12:15:02 +0900

    fix(activityLog): aop 로직 추가
    - 로그인 시 응답 헤더에서 id 추출 로직 추가

--- src/main/java/com/example/outsourcingproject/global/aop/aspect/ActivityLogAspect.java
+++ src/main/java/com/example/outsourcingproject/global/aop/aspect/ActivityLogAspect.java
@@ -3,17 +3,26 @@ package com.example.outsourcingproject.global.aop.aspect;
 import com.example.outsourcingproject.global.aop.annotation.LogActivity;
 import com.example.outsourcingproject.global.aop.event.ActivityLogPublisher;
 import com.example.outsourcingproject.global.aop.event.dto.ActivityLogEventDto;
+import com.example.outsourcingproject.global.enums.ActivityType;
 import com.example.outsourcingproject.global.enums.RequestMethod;
+import com.example.outsourcingproject.global.security.JwtTokenProvider;
 import com.example.outsourcingproject.global.security.UserPrincipal;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
+@Slf4j
 @Aspect
 @Component
 @RequiredArgsConstructor
@@ -23,11 +32,14 @@ public class ActivityLogAspect {
 
     private final HttpServletRequest httpServletRequest;
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Around("@annotation(logActivity)")
     public Object logActivity(ProceedingJoinPoint joinPoint, LogActivity logActivity) throws Throwable {
 
-        Object result = joinPoint.proceed();
+        log.info("logActivity aop 실행");
 
+        ResponseEntity<?> result = (ResponseEntity<?>) joinPoint.proceed();
 
         // userId
         Long userId = null;
@@ -37,6 +49,16 @@ public class ActivityLogAspect {
             if (principal instanceof UserPrincipal userPrincipal) {
                 userId = userPrincipal.getId();
             }
+        }
+
+        // 로그인 시 응답 헤더에서 id 추출
+        if(logActivity.type().equals(ActivityType.USER_LOGGED_IN)) {
+            HttpHeaders headers = result.getHeaders();
+            String bearerToken = headers.getFirst("Authorization");
+            String token = Objects.requireNonNull(bearerToken).substring(7);
+
+            Claims claims = jwtTokenProvider.parseToken(token);
+            userId = Long.parseLong(claims.getSubject());
         }
 
         if(userId == null) {
