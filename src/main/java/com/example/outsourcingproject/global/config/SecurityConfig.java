5bf016e435fd13cf8b816c8d3a8e352f5c6f097f
Mybread2 <ckwnsgh4@naver.com>
2025-06-17 12:24:18 +0900

    fix: 로그인 시에 profile 로 안넘어가지는 버그 해결 - securityConfig에 cors 관련 설정 추가

--- src/main/java/com/example/outsourcingproject/global/config/SecurityConfig.java
+++ src/main/java/com/example/outsourcingproject/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,6 +34,8 @@ public class SecurityConfig {
         log.info("SecurityConfig 실행됨");
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+
+                .cors(Customizer.withDefaults())
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
