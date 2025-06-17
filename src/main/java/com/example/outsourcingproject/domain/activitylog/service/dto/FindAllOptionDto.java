8b076d3da716fc5cdbb538e216c60ede2ff34a4e
huouvcti <huouvcti@gamil.com>
2025-06-17 16:51:27 +0900

    feat(ActivityLog): Controller -> Service 간 DTO 생성

---
+++ src/main/java/com/example/outsourcingproject/domain/activitylog/service/dto/FindAllOptionDto.java
@@ -0,0 +1,20 @@
+package com.example.outsourcingproject.domain.activitylog.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class FindAllOptionDto {
+    private Pageable pageable;
+
+    private Long userId;
+    private String activityType;
+    private Integer targetId;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
