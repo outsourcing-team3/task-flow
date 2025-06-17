aeaed9bb417fc502e631d49708671e8e5e1b0fba
syuare-dev <tachunsa13@gmail.com>
2025-06-17 23:12:29 +0900

    Refactor(Task): Priority - Enum 으로 관리
    > Priority.java
    - enum 생성
     > - LOW -> 낮음
     > - MEDIUM -> 중간
     > - HIGH -> 높음
    - 요청 값으로 영어 / 한글 둘 다 입력 시 동작하도록 구현
    - 응답 값에는 영어로 조회되도록 구현
    
    > Priority 를 enum 으로 관리함에 따라 영향받는 코드 리팩토링
      - Task
      - TaskCreateResponseDto
      - TaskCreateRequestDto
      - TaskReadResponseDto

--- src/main/java/com/example/outsourcingproject/domain/task/dto/TaskCreateResponseDto.java
+++ src/main/java/com/example/outsourcingproject/domain/task/dto/TaskCreateResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.outsourcingproject.domain.task.dto;
 
 import com.example.outsourcingproject.domain.task.entity.Task;
+import com.example.outsourcingproject.domain.task.enums.Priority;
 import com.example.outsourcingproject.domain.task.enums.TaskStatus;
 import lombok.Getter;
 
@@ -29,12 +30,12 @@ public class TaskCreateResponseDto {
 
     private LocalDateTime updatedAt;
 
-    public TaskCreateResponseDto(String title, String description, TaskStatus status, LocalDateTime deadline, String priority, String assignee, String creator, LocalDateTime startedAt, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public TaskCreateResponseDto(String title, String description, TaskStatus status, LocalDateTime deadline, Priority priority, String assignee, String creator, LocalDateTime startedAt, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.title = title;
         this.description = description;
         this.status = status.name();
         this.deadline = deadline;
-        this.priority = priority;
+        this.priority = priority.name();
         this.assignee = assignee;
         this.creator = creator;
         this.startedAt = startedAt;
