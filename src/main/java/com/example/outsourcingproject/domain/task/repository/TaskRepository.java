8a42f2b80d01a0ceb367c04acb7a6b527c07439d
syuare-dev <tachunsa13@gmail.com>
2025-06-18 21:44:24 +0900

    Refactor(Task): 태스크 조회 기능 - 페이징 처리
    - List 로 구현된 조회 기능들을 페이징 처리하여 응답값으로 반환하도록 리팩토링
    
    > TaskRepository.java
    - List 타입을 Page 타입으로 변경
    - 매개변수에 Pageable 추가
    
    > TaskService.java
    - 페이지 공통 응답 DTO PagedResponse 활용
    - Page 처리 후 Page<TaskReadResponseDto> 타입으로 변환 후 반환
    
    > TaskController.java
    - page, size 를 파라미터로 요청받도록 구현
    - page, size 기본값 설정: page->1, size->10
    - 서비스 레이어 전달 시 page, size 값도 전달하도록 수정

--- src/main/java/com/example/outsourcingproject/domain/task/repository/TaskRepository.java
+++ src/main/java/com/example/outsourcingproject/domain/task/repository/TaskRepository.java
@@ -2,24 +2,25 @@ package com.example.outsourcingproject.domain.task.repository;
 
 import com.example.outsourcingproject.domain.task.entity.Task;
 import com.example.outsourcingproject.domain.task.enums.TaskStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     // 삭제되지 않은(Task.isDeleted = false) Task 를 모두 조회
-    List<Task> findAllByIsDeletedFalse();
+    Page<Task> findAllByIsDeletedFalse(Pageable pageable);
 
     // 삭제되지 않은 Task 중 Status 가 특정 값인 Task 조회
-    List<Task> findAllByIsDeletedFalseAndStatus(TaskStatus status);
+    Page<Task> findAllByIsDeletedFalseAndStatus(TaskStatus status, Pageable pageable);
 
     // 제목(title) 로 검색
-    List<Task> findByTitleContaining(String title);
+    Page<Task> findByTitleContaining(String title, Pageable pageable);
 
     // 설명(description) 으로 검색
-    List<Task> findByDescriptionContaining(String description);
+    Page<Task> findByDescriptionContaining(String description, Pageable pageable);
 
     // 삭제되지 않은(Task.isDeleted = false) Task 중 taskId 로 Task 를 조회
     Optional<Task> findByIdAndIsDeletedFalse(Long taskId);
