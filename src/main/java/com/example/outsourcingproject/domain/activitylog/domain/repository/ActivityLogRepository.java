635d0b9fdc5596acf94dd960f80487263bd0d1fd
huouvcti <huouvcti@gamil.com>
2025-06-19 11:57:54 +0900

    fix(ActivityLog): 로그 전체 조회 param 값 변경

--- src/main/java/com/example/outsourcingproject/domain/activitylog/domain/repository/ActivityLogRepository.java
+++ src/main/java/com/example/outsourcingproject/domain/activitylog/domain/repository/ActivityLogRepository.java
@@ -2,7 +2,6 @@ package com.example.outsourcingproject.domain.activitylog.domain.repository;
 
 import com.example.outsourcingproject.domain.activitylog.domain.model.ActivityLog;
 import com.example.outsourcingproject.global.enums.ActivityType;
-import com.example.outsourcingproject.global.enums.TargetType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,7 +18,7 @@ public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> 
         SELECT a FROM ActivityLog a
         WHERE (:userId IS NULL OR a.auth.id = :userId)
           AND (:activityType IS NULL OR a.activityType = :activityType)
-          AND (:targetType IS NULL OR a.targetType = :targetType)
+          AND (:targetId IS NULL OR a.targetId = :targetId)
           AND (:startDate IS NULL OR a.createdAt >= :startDate)
           AND (:endDate IS NULL OR a.createdAt <= :endDate)
         ORDER BY a.createdAt DESC
@@ -27,7 +26,7 @@ public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> 
     Page<ActivityLog> findActivityLogs(
             @Param("userId") Long userId,
             @Param("activityType") ActivityType activityType,
-            @Param("targetType") TargetType targetType,
+            @Param("targetId") Long targetId,
             @Param("startDate") LocalDateTime startDate,
             @Param("endDate") LocalDateTime endDate,
             Pageable pageable
