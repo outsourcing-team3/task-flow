6fdd33dce8c54356fc1bb8989c702999a05d8db4
zeroColaa <sinyoung556@gmail.com>
2025-06-19 10:07:41 +0900

    refactor: DashboardController 응답 타입 ResponseEntity로 변경

--- src/main/java/com/example/outsourcingproject/domain/dashboard/controller/DashboardController.java
+++ src/main/java/com/example/outsourcingproject/domain/dashboard/controller/DashboardController.java
@@ -8,6 +8,7 @@ import com.example.outsourcingproject.domain.dashboard.service.DashboardService;
 import com.example.outsourcingproject.global.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,18 +24,18 @@ public class DashboardController {
     private final DashboardService dashboardService;
 
     @GetMapping("/dashboard/statistics")
-    public ApiResponse<DashboardStatisticsDto> statistics(
+    public ResponseEntity<ApiResponse<DashboardStatisticsDto>> statistics(
             @RequestParam(required = false)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-        return ApiResponse.success(dashboardService.getWeeklyStatistics(date), "통계 조회 성공");
+        return  ResponseEntity.ok(ApiResponse.success(dashboardService.getWeeklyStatistics(date), "통계 조회 성공"));
     }
 
 
     @GetMapping("/dashboard/my-tasks")
-    public ApiResponse<List<TodayTaskItemDto>> getMyTodayTasks(
+    public ResponseEntity<ApiResponse<List<TodayTaskItemDto>>> getMyTodayTasks(
             @AuthenticationPrincipal UserPrincipal principal) {
 
-        return ApiResponse.success(dashboardService.getMyTodayTasks(principal.getId()), "오늘의 태스크 조회 성공");
+        return  ResponseEntity.ok(ApiResponse.success(dashboardService.getMyTodayTasks(principal.getId()), "오늘의 태스크 조회 성공"));
     }
 
 //    @GetMapping("/dashboard/my-tasks")
@@ -47,33 +48,33 @@ public class DashboardController {
 //    }
 
     @GetMapping("/dashboard/trend/week")
-    public ApiResponse<List<DailyTaskTrendDto>> weeklyTrend(
+    public  ResponseEntity<ApiResponse<List<DailyTaskTrendDto>>> weeklyTrend(
             @RequestParam(required = false)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-        return ApiResponse.success(dashboardService.getWeeklyTrend(date), "주간 트렌드 조회 성공");
+        return ResponseEntity.ok(ApiResponse.success(dashboardService.getWeeklyTrend(date), "주간 트렌드 조회 성공"));
     }
     @GetMapping("/dashboard/status-ratio")
-    public ApiResponse<TaskStatusRatioDto> statusRatio() {
-        return ApiResponse.success(dashboardService.getStatusRatio(), "작업 상태 분포 조회 성공");
+    public  ResponseEntity<ApiResponse<TaskStatusRatioDto>> statusRatio() {
+        return ResponseEntity.ok(ApiResponse.success(dashboardService.getStatusRatio(), "작업 상태 분포 조회 성공"));
     }
 
 
     @GetMapping("/dashboard/progress-ratio")
-    public ApiResponse<ProgressRatioDto> progressRatio(
+    public  ResponseEntity<ApiResponse<ProgressRatioDto>> progressRatio(
             @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        return ApiResponse.success(dashboardService.getProgressRatio(userPrincipal.getId()), "진행률 통계 조회 성공");
+        return ResponseEntity.ok(ApiResponse.success(dashboardService.getProgressRatio(userPrincipal.getId()), "진행률 통계 조회 성공"));
     }
 
 
     @GetMapping("/dashboard/trend/month")
-    public ApiResponse<List<MonthlyTaskTrendDto>> monthTrend() {
-        return ApiResponse.success(dashboardService.getMonthlyTrend(), "월간 작업 추세");
+    public  ResponseEntity<ApiResponse<List<MonthlyTaskTrendDto>>> monthTrend() {
+        return ResponseEntity.ok(ApiResponse.success(dashboardService.getMonthlyTrend(), "월간 작업 추세"));
     }
 
 
     @GetMapping("/dashboard/activity-feed")
-    public ApiResponse<List<ActivityFeedDto>> feed() {
-        return ApiResponse.success(dashboardService.getAcitivityFeed(), "활동 피드를 조회했습니다.");
+    public  ResponseEntity<ApiResponse<List<ActivityFeedDto>>> feed() {
+        return ResponseEntity.ok(ApiResponse.success(dashboardService.getAcitivityFeed(), "활동 피드를 조회했습니다."));
     }
 
 }
