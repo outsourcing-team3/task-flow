5fe55ab61d0312ff0d02a68ffd1923d9f44739cf
huouvcti <huouvcti@gamil.com>
2025-06-19 16:50:18 +0900

    fix: DDL 수정

--- DDL.sql
+++ DDL.sql
@@ -93,9 +93,12 @@ CREATE TABLE tasks
     status      VARCHAR(20) COMMENT '태스크 상태',
     started_at  DATETIME NOT NULL COMMENT '태스크 시작일',
 
+    -- BaseEntity 공통 필드들
     is_deleted BOOLEAN NOT NULL DEFAULT FALSE COMMENT '삭제 여부',
     created_at DATETIME NOT NULL COMMENT '생성일',
     updated_at DATETIME NOT NULL COMMENT '수정일',
+    created_by BIGINT COMMENT '생성자 ID',
+    last_modified_by BIGINT COMMENT '수정자 ID',
     deleted_at DATETIME COMMENT '삭제일',
 
     PRIMARY KEY (id),
@@ -110,12 +113,10 @@ CREATE TABLE comments (
     content VARCHAR(255) NOT NULL COMMENT '댓글 내용',
     task_id BIGINT NOT NULL COMMENT '댓글이 달린 태스크의 식별자',
 
-    is_deleted  BOOLEAN  NOT NULL DEFAULT FALSE COMMENT '삭제 여부',
-    created_at  DATETIME NOT NULL COMMENT '생성일',
-    updated_at  DATETIME NOT NULL COMMENT '수정일',
-    created_by BIGINT COMMENT '생성자 ID (AuditorAware)',
-    last_modified_by BIGINT COMMENT '수정자 ID (AuditorAware)',
-    deleted_at  DATETIME COMMENT '삭제일',
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE COMMENT '삭제 여부',
+    created_at DATETIME NOT NULL COMMENT '생성일',
+    updated_at DATETIME NOT NULL COMMENT '수정일',
+    deleted_at DATETIME COMMENT '삭제일',
 
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id),
